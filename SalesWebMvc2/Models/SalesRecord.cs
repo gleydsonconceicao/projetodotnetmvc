f87804b308942d7940e1308db642b681a5b4281c
Gleydson Paulo <glayck@hotmail.com>
2022-08-30 15:45:01 -0300

    Subindo projeto v2 a primeira vez

---
+++ SalesWebMvc2/Models/SalesRecord.cs
@@ -0,0 +1,37 @@
+﻿using SalesWebMvc2.Models.Enuns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc2.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amaunt { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amaunt, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amaunt = amaunt;
+            Status = status;
+            Seller = seller;
+        }
+
+        public SalesRecord(DateTime date, double amaunt, SaleStatus status, Seller seller)
+        {
+            Date = date;
+            Amaunt = amaunt;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SalesWebMvc2/Services/SellerService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc2.Data;
+using SalesWebMvc2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc2.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvc2Context _context;
+        public SellerService(SalesWebMvc2Context context)
+        {
+            _context = context;
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(seller => seller.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+    }
+}
